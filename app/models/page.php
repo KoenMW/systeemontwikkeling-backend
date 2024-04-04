0305c1357a8400a79ee2ebd0278a46f2744dbd6f
LukoGi <lukopecco@gmail.com>
2024-04-04 17:21:16 +0200

    Page NULL

--- app/models/page.php
+++ app/models/page.php
@@ -6,11 +6,7 @@ class Page
 {
     public int $id;
     public ?int $parentId;
-<<<<<<< HEAD
-    public string $name;
-=======
     public ?string $name;
->>>>>>> main
     public ?string $intro;
     public ?string $picture;
     public ?array $cards;
